2076d1cb421bd8d74ad9ec097757400a0d9e83ac
عبدالرحمن عاشور <137439549+Abdelrahman-Ashour@users.noreply.github.com>
2025-05-04 04:26:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from pages import idea_finder, customer_service, history
 
-st.set_page_config(page_title="Startup Idea Finder", layout="wide")
+st.set_page_config(page_title="IDEA AI", layout="wide")
 
 pages = {
-    "Idea Finder": idea_finder.app,  # تأكد من استيراد app بشكل صحيح
+    "Idea Finder": idea_finder.app,  # تأكد من أن app موجود في idea_finder.py
     "Customer Service": customer_service.app,
     "History": history.app,
 }
