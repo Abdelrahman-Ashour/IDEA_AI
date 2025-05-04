da3ca714c4ddd0c5a44458b37351f019eba9a7e0
عبدالرحمن عاشور <137439549+Abdelrahman-Ashour@users.noreply.github.com>
2025-05-04 04:39:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from pages import customer_service
 from pages import history
-from pages import idea_finder
+from pages import idea
 
 st.set_page_config(page_title="IDEA AI", layout="wide")
 
 pages = {
     "Customer Service": customer_service.app,
     "History": history.app,
-    "Idea Finder": idea_finder.app,  # تأكد من أن app موجود في idea_finder.py
+    "Idea Finder": idea.app,  # تأكد من أن app موجود في idea_finder.py
 }
 
 st.sidebar.title("Navigation")
